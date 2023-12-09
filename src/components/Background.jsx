4275e69e8ce9edfce3351c4ea26d2949ced1544d
Ram Sah <mohan2036@gmail.com>
2023-12-09 11:32:39 -0500

    added text

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -5,7 +5,7 @@ const Background = () => {
         <>
             <div className=' fixed w-full h-screen z-[2]'>
                 <h1 className=' text-[10vw] leading-none tracking-tighter absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 font-semibold'>Hello</h1>
-                <div className=" absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 font-semibold text-xl ">Hello There</div>
+                <div className=" absolute top-[5%] w-full py-10 flex justify-center text-zinc-400 font-semibold text-xl ">Hello There to move these cards just drag them them</div>
 
             </div>
         </>
